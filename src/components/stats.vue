0e0b2483ed175a135a941bb289c897066f3505bf
LlanddewiLovesYou <idelduca@stridenyc.com>
2018-09-16 01:04:01 -0400

    Finished project, ready for push

---
+++ src/components/stats.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="stats">
+    <h1>How Many Links Are There</h1>
+    <p>There are currently {{ countLinks }} links.</p>
+    <button v-on:click='removeAllLinks'>Remove All Links!</button>
+    <p>{{ msg }}</p>
+  </div>
+
+
+</template>
+
+<script>
+import { mapGetters, mapActions, mapMutations } from 'vuex'
+
+export default {
+  name: "stats",
+  data(){
+    return {
+      msg: ""
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'countLinks'
+    ]),
+  },
+  methods: {
+    ...mapMutations([
+      'REMOVE_ALL'
+    ]),
+    ...mapActions([
+      'removeAll'
+    ]),
+    //class component methods... click handlers etc.
+    removeAllLinks() {
+      this.removeAll().then(() => {
+        this.msg = "All Links Have Been REMOVED!"
+      })
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,45 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    title: "Links R Us!",
+    links: [
+      'http://www.coursetro.com',
+      'http://www.google.com',
+      'http://www.facebook.com',
+    ]
+  },
+  // Getters: perform calculations on data dynamically
+  getters: {
+    countLinks: (state) => {
+      return state.links.length
+    }
+  },
+  //Mutations: Change state data synchronously, think this.setState()
+  mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1)
+    },
+    REMOVE_ALL: (state) => {
+      state.links = []
+    }
+  },
+  //Actions: Change the state Asynchronously, much like thunk
+  // NB: Use quotation marks in the commit. Context gives you access to commit function
+  actions: {
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link)
+    },
+    removeAll: ({ commit }) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL')
+          resolve()
+        }, 1500)
+      })
+    }
+  }
 });
